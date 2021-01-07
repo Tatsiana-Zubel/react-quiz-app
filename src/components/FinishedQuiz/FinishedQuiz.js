c6935a5c5437416ef2abc580f1784e0e7067f607
tatsianazubel <tatsiana.zubel@icloud.com>
2021-01-07 16:26:51 +0200

    Installed react-router-dom library. Added routes for new containers.

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import classes from './FinishedQuiz.module.css'
 import Button from "../UI/Button/Button";
+import {Link} from 'react-router-dom'
 
 const FinishedQuiz  = props => {
     const successCount = Object.keys(props.results).reduce((total,key) => {
@@ -35,7 +36,9 @@ const FinishedQuiz  = props => {
             <p>Correct answers {successCount} from {props.quiz.length}</p>
             <div>
                 <Button onClick={props.onRetry} type="primary">Try again</Button>
-                <Button type="success">Go to the test list</Button>
+                <Link to="/">
+                    <Button type="success" >Go to the test list</Button>
+                </Link>
 
             </div>
         </div>
